5cdeae7c6fa5d82da0933a60456f37f9eeaba713
Sharkony <alexeysharkony@gmail.com>
2022-08-11 21:09:04 +0500

    init: solved brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ const bracketMatch = {
+  "(": ")",
+  "[": "]",
+  "{": "}",
+  "1": "2",
+  "3": "4",
+  "5": "6",
+ };
+ 
+ const similarBrackets = ['|', '7', '8'];
+
+ let checkArr = [];
+ let openBracketArr = Object.keys(bracketMatch);
+ let closedBracketArr = Object.values(bracketMatch);
+
+ for (let i=0; i < str.length; i++) {
+  let currentBracket = str[i];
+  if (similarBrackets.includes(currentBracket)) {
+    if (checkArr.includes(currentBracket)){
+      checkArr.pop()
+    } else {checkArr.push(currentBracket)
+    }
+  }
+
+  if (openBracketArr.includes(currentBracket)) {
+    checkArr.push(currentBracket);
+  } else
+  if (closedBracketArr.includes(currentBracket)){
+    let lastBracket = checkArr.pop()
+    if (bracketMatch[lastBracket] !== currentBracket) {
+      return false
+    }
+  }
+ }
+  return (checkArr.length == 0)
+ 
 }
